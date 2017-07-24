4ef6407b9d24c461f5c1e3aae0652a781b113c98
Sarah Basinger <sarrey@gmail.com>
2017-07-23 22:51:22 -0400

    Update styles when hovering over an auction item on home page

--- front-end/src/components/AuctionItem.js
+++ front-end/src/components/AuctionItem.js
@@ -8,13 +8,13 @@ class AuctionItem extends Component {
 		var auctionLink = '/auction/'+auctionItem.id;
 		return(
 		<div className="col-sm-4 col-xs-12">
-			<div className="col-xs-12 product-wrapper">
+			<Link to={auctionLink}><div className="col-xs-12 product-wrapper">
 				<div className="col-xs-12">
-					<h4><Link to={auctionLink}>{auctionItem.title}</Link></h4>
-					<div className="auction-item"><img src={auctionItem.url} role="presentation"/></div>
+					<h4 alt={auctionItem.title} title={auctionItem.title}>{auctionItem.title}</h4>
 					<p>Current bid: <span className="orange">${parseFloat(auctionItem.current_bid).toFixed(2)}</span></p>
+					<div className="auction-item"><img src={auctionItem.url} role="presentation"/></div>
 				</div>
-			</div>
+			</div></Link>
 		</div>
 		)
 	}
